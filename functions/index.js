100e7a6aea0461f87db6cf1474ec4034ebc057a2
Jacky Tang <jackytck@gmail.com>
2017-09-03 00:50:26 +0800

    :sparkles: setup firebase service account

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,12 @@
+const admin = require('firebase-admin')
 const functions = require('firebase-functions')
+const createUser = require('./create_user')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!')
+const serviceAccount = require('path/to/serviceAccountKey.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://one-time-password-9cc06.firebaseio.com'
 })
 
-exports.goodbye = functions.https.onRequest((request, response) => {
-  response.send('Goodbye!')
-})
+exports.createUser = functions.https.onRequest(createUser)
